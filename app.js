eb9ee1a8733913becfcd09735be17036db373597
David Ker <snappy316@gmail.com>
2015-04-13 13:54:27 -0700

    4.3 Creating an Element Directive

--- app.js
+++ app.js
@@ -35,6 +35,13 @@
     };
   });
 
+  app.directive('productDescription', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-description.html'
+    };
+  });
+
   var gems = [
     {
       name: 'Azurite',
