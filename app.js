cee43abe92f5b9c3daca80acc68c92cc6127e33b
David Ker <snappy316@gmail.com>
2015-04-13 12:44:24 -0700

    2.9 Creating Gallery Controller

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 (function() {
   var app = angular.module('gemStore', []);
 
+  app.controller('GalleryController', function(){
+    this.current = 0;
+    
+    this.setCurrent = function(val) {
+      this.current = val || 0;
+    };
+  });
+
   app.controller('StoreController', function(){
     this.products = gems;
   });
